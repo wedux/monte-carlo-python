608868e1ed5854475a45bf27eea8cc5dd24590c9
wedux <88342381+wedux@users.noreply.github.com>
2022-02-13 12:59:54 +0100

    pierwszy commit

---
+++ monte carlo.py
@@ -0,0 +1,35 @@
+from turtle import *
+from random import *
+from math import sqrt
+
+def kwadrat(dl):
+    for i in range(4):
+        lt(90)
+        fd(dl)
+speed(0)
+setup(1200, 1000)
+title("Metoda Monte Carlo")
+kwadrat(400)
+lt(90)
+up()
+fd(400/2)
+down()
+circle(400/2)
+
+
+ht()
+tracer(5)
+for i in range(1, 51):
+    pktCircle = 0
+    up()
+    x = randint(-400, 0)
+    y = randint(0, 400)
+    goto(x, y)
+    down()
+
+    
+    if sqrt(x**2 + y**2) <= 1:
+        dot(5, "blue")
+    else:
+        dot(5, "red")
+
